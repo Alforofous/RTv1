2c86d8ec69bbaa5dbd49307e46c2ff36cf504a2d
Alforofus <davidm.22@hotmail.com>
2022-07-25 16:52:14 +0300

    Image2 size decreased. Will only show now RTv1 params.

--- sources/image2.c
+++ sources/image2.c
@@ -6,7 +6,7 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 10:44:56 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/07/20 10:45:30 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/07/25 16:27:36 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 void	draw_image2(t_utils *utils)
 {
-	int	pos[2];
+	t_2i	coords;
 
-	pos[0] = utils->curr_img->dim.width / 10;
-	pos[1] = utils->curr_img->dim.height / 40;
-	if (utils->sel_object != NULL)
-	{
-		draw_rectf(&(t_pxl_func){&ft_pixel_put, utils->curr_img},
-			&(t_2i){pos[0], pos[1]}, &(t_2i){pos[0] * 9, pos[1] * 9}, utils->sel_object->color);
-	}
+	coords.x = (int)(utils->curr_img->dim.width * 0.0);
+	coords.y = (int)(utils->curr_img->dim.height * 0.0);
+	coords = display_str(utils, coords, utils->font2, "FOV:");
+	display_int(utils, coords, utils->font2, (int)utils->proj.fov);
 	draw_rect(&(t_pxl_func){&ft_pixel_put, utils->curr_img},
 		&(t_2i){0, 0}, &(t_2i){utils->curr_img->dim.width - 1,
 		utils->curr_img->dim.height - 1}, 0xFFDD45);
