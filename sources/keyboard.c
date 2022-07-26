1f2408124619ec882550f0cf583e7274192288f8
Alforofus <davidm.22@hotmail.com>
2022-07-26 10:42:32 +0300

    Screen will now only render when keys that need to rerender the screen are pressed. It ignores all the other ones.

--- sources/keyboard.c
+++ sources/keyboard.c
@@ -6,7 +6,7 @@
 /*   By: dmalesev <dmalesev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:01:42 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/07/25 16:41:43 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/07/26 10:27:37 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,18 @@ void	toggle_keys(t_utils *utils, int key)
 {
 	if (key == DOT)
 		utils->visual_rays += 1;
-	if (utils->visual_rays == 3)
+	else if (utils->visual_rays == 3)
 		utils->visual_rays = 0;
-	if (key == T)
+	else if (key == T)
 		init_camera(utils);
-	if (key == R)
+	else if (key == R)
 		utils->render *= -1;
+	else
+		return ;
 	utils->rmatrix_x = init_rmatrix_x(utils->rot.x);
 	utils->rmatrix_y = init_rmatrix_y(utils->rot.y);
 	utils->rmatrix_z = init_rmatrix_z(utils->rot.z);
+	render_screen(utils);
 }
 
 void	fov_keys(t_utils *utils, int key)
@@ -34,7 +37,10 @@ void	fov_keys(t_utils *utils, int key)
 		utils->proj.fov += 1;
 	else if (key == NUM_MINUS && utils->proj.fov > 10)
 		utils->proj.fov -= 1;
+	else
+		return ;
 	utils->proj.fov_rad = (float)(1 / tan(utils->proj.fov / 2 / 180 * PI));
+	render_screen(utils);
 }
 
 static void	moving_object(t_utils *utils, t_object *sel_object, int key)
@@ -42,37 +48,37 @@ static void	moving_object(t_utils *utils, t_object *sel_object, int key)
 	if (key == UP)
 		sel_object->origin = add_vectors(&sel_object->origin,
 			&utils->cam.dir.forward);
-	if (key == LEFT)
+	else if (key == LEFT)
 		sel_object->origin = add_vectors(&sel_object->origin,
 			&utils->cam.dir.left);
-	if (key == DOWN)
+	else if (key == DOWN)
 		sel_object->origin = add_vectors(&sel_object->origin,
 			&utils->cam.dir.back);
-	if (key == RIGHT)
+	else if (key == RIGHT)
 		sel_object->origin = add_vectors(&sel_object->origin,
 			&utils->cam.dir.right);
+	else
+		return ;
+	render_screen(utils);
 }
 
 void	moving_camera(t_utils *utils, int key)
 {
 	if (key == W)
-		utils->cam.origin = add_vectors(&utils->cam.origin,
-			&utils->cam.dir.forward);
-	if (key == A)
-		utils->cam.origin = add_vectors(&utils->cam.origin,
-			&utils->cam.dir.left);
-	if (key == S)
-		utils->cam.origin = add_vectors(&utils->cam.origin,
-			&utils->cam.dir.back);
-	if (key == D)
-		utils->cam.origin = add_vectors(&utils->cam.origin,
-			&utils->cam.dir.right);
-	if (key == SPACE)
-		utils->cam.origin = add_vectors(&utils->cam.origin,
-			&utils->cam.dir.down);
-	if (key == L_SHIFT)
-		utils->cam.origin = add_vectors(&utils->cam.origin,
-			&utils->cam.dir.up);
+		utils->cam.origin = add_vectors(&utils->cam.origin, &utils->cam.dir.forward);
+	else if (key == A)
+		utils->cam.origin = add_vectors(&utils->cam.origin, &utils->cam.dir.left);
+	else if (key == S)
+		utils->cam.origin = add_vectors(&utils->cam.origin, &utils->cam.dir.back);
+	else if (key == D)
+		utils->cam.origin = add_vectors(&utils->cam.origin, &utils->cam.dir.right);
+	else if (key == SPACE)
+		utils->cam.origin = add_vectors(&utils->cam.origin, &utils->cam.dir.down);
+	else if (key == L_SHIFT)
+		utils->cam.origin = add_vectors(&utils->cam.origin, &utils->cam.dir.up);
+	else
+		return ;
+	render_screen(utils);
 }
 
 int	key_down(int key, void *param)
@@ -84,9 +90,9 @@ int	key_down(int key, void *param)
 	if (key == ESC)
 		close_prog(utils, "Exited program succesfully using ESC.", 1);
 	moving_camera(utils, key);
-	moving_object(utils, utils->sel_object, key);
+	if (utils->sel_object != NULL)
+		moving_object(utils, utils->sel_object, key);
 	fov_keys(utils, key);
 	toggle_keys(utils, key);
-	render_screen(utils);
 	return (0);
 }
