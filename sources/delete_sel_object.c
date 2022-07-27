9b9b926213be9a01a22680d6a5fa0fb6925024ad
Alforofus <davidm.22@hotmail.com>
2022-07-27 14:55:05 +0300

    New file that contains function to remove an object from a list and reattach linked list nodes between deleted object's neighbours.

---
+++ sources/delete_sel_object.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   delete_sel_object.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 11:10:28 by dmalesev          #+#    #+#             */
+/*   Updated: 2022/07/27 14:14:28 by dmalesev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+static void	ft_lstcnct(t_list **from, t_list **to)
+{
+	if (*from)
+		(*from)->next = *to;
+}
+
+static void	del_object(void *content, size_t content_size)
+{
+	ft_bzero(content, content_size);
+	free(content);
+}
+
+void	delete_sel_object(t_utils *utils, t_list **objects)
+{
+	t_list		*temp;
+	t_list		*prev;
+	t_object	*object;
+
+	temp = *objects;
+	prev = NULL;
+	while (*objects != NULL)
+	{
+		object = (t_object *)(*objects)->content;
+		if (object == utils->sel_object)
+		{
+			if (prev == NULL)
+				temp = (*objects)->next;
+			else
+				ft_lstcnct(&prev, &(*objects)->next);
+			ft_lstdelone(objects, &del_object);
+		}
+		prev = *objects;
+		if (*objects != NULL)
+			*objects = (*objects)->next;
+	}
+	*objects = temp;
+	utils->sel_object = NULL;
+	utils->closest_object = NULL;
+	render_screen(utils);
+}
