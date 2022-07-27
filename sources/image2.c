1f9b53863ca5964829c1ef0c3ecbb9e3c3904bea
Alforofus <davidm.22@hotmail.com>
2022-07-27 14:49:27 +0300

    Object count param displayed in image2 now.

--- sources/image2.c
+++ sources/image2.c
@@ -6,7 +6,7 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 10:44:56 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/07/25 16:27:36 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/07/27 14:33:39 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ void	draw_image2(t_utils *utils)
 
 	coords.x = (int)(utils->curr_img->dim.width * 0.0);
 	coords.y = (int)(utils->curr_img->dim.height * 0.0);
-	coords = display_str(utils, coords, utils->font2, "FOV:");
+	coords = display_str(utils, coords, utils->font2, "FOV: ");
 	display_int(utils, coords, utils->font2, (int)utils->proj.fov);
+	coords.x = (int)(utils->curr_img->dim.width * 0.0);
+	coords.y = (int)(utils->curr_img->dim.height * 0.25);
+	coords = display_str(utils, coords, utils->font2, "OBJECT COUNT: ");
+	display_int(utils, coords, utils->font2, (int)ft_lstsize(utils->objects));
 	draw_rect(&(t_pxl_func){&ft_pixel_put, utils->curr_img},
 		&(t_2i){0, 0}, &(t_2i){utils->curr_img->dim.width - 1,
 		utils->curr_img->dim.height - 1}, 0xFFDD45);
