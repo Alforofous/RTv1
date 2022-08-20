a0197677401bd8e7c006ddb2d27f83c3a23a8caf
Alforofus <davidm.22@hotmail.com>
2022-08-20 12:13:46 +0300

    Added outline to delete object button. Changed color to more saturated red.

--- sources/image5.c
+++ sources/image5.c
@@ -6,7 +6,7 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 15:25:31 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/08/19 13:30:52 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/08/20 12:12:27 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ void	draw_image5(t_utils *utils)
 	coords[1].y = (int)(utils->curr_img->dim.height * 0.7);
 	draw_rectf(&(t_pxl_func){&put_pixel, utils->curr_img},
 		&(t_2i){0, 0}, &(t_2i){utils->curr_img->dim.width - 1,
-		utils->curr_img->dim.height - 1}, 0x780000);
+		utils->curr_img->dim.height - 1}, 0xCD0000);
+	draw_rect(&(t_pxl_func){&put_dot, utils},
+		&(t_2i){0, 0}, &(t_2i){utils->curr_img->dim.width - 1,
+		utils->curr_img->dim.height - 1}, 0xFFFFFF);
 	draw_line(&(t_pxl_func){&put_dot, utils},
 		&(t_line){coords[0].x, coords[0].y, coords[1].x, coords[1].y},
 		0xFFFFFF, 0xFFFFFF);
