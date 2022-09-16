ad6a27a42fbc54986fd01d1b2e8a82b5682daf19
Alforofus <davidm.22@hotmail.com>
2022-09-16 14:55:42 +0300

    Image 8 now calls for image iterate function which iterates rgb_slider function to draw it through whole image.

--- sources/image8.c
+++ sources/image8.c
@@ -6,15 +6,32 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 14:55:41 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/09/15 16:56:06 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/09/16 14:25:44 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
+static void	rgb_iter(t_img *img, t_uint (*f)(t_img *, t_2i *))
+{
+	t_2i	coords;
+
+	coords.y = 0;
+	while (coords.y <= img->dim.height)
+	{
+		coords.x = 0;
+		while (coords.x <= img->dim.width)
+		{
+			f(img, &coords);
+			coords.x += 1;
+		}
+		coords.y += 1;
+	}
+}
+
 void	draw_image8(t_utils *utils)
 {
-	draw_rgb_slider(utils->curr_img);
+	rgb_iter(utils->curr_img, &rgb_slider);
 	draw_rect(&(t_pxl_func){&put_dot, utils},
 		&(t_2i){0, 0}, &(t_2i){utils->curr_img->dim.width - 2,
 		utils->curr_img->dim.height - 2}, 0xFFFFFF);
