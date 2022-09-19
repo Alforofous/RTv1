994b9f497af3f0dbc7e7842008d9dff5960d40ad
Alforofus <davidm.22@hotmail.com>
2022-09-19 09:50:31 +0300

    Shadows rays no longer travel from light to object with the check that ray and light hit the same object. They now travel from camera hit point to light (reversed). This solves a bug if both camera and light hit the same object, but from different side. Still working on it tho. Should be done soon.

--- sources/image1.c
+++ sources/image1.c
@@ -6,7 +6,7 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 10:41:05 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/09/16 14:52:05 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/09/17 12:29:54 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,9 +75,7 @@ static t_3f	intersect(t_utils *utils, t_3f *ray, t_3f *ray_origin, t_img *img, t
 				*point_hit = scale_vector(*ray, (float)t[1].x);
 				*point_hit = add_vectors(*point_hit, *ray_origin);
 				t[0] = t[1];
-				if (object->type == 1)
-					normal = (t_3f){0.0f, 0.0f, 0.0f};
-				if (object->type == 1)
+				if (object->type == 1 || object->type == 0)
 				{
 					normal = normalize_vector(subtract_vectors(*point_hit, object->origin));
 					if (t[0].x == t[0].y)
@@ -108,18 +106,9 @@ static t_3f	intersect(t_utils *utils, t_3f *ray, t_3f *ray_origin, t_img *img, t
 						normal = scale_vector(normal, -1.0f);
 				}
 				utils->closest_object = object;
-				if (utils->render == -1)
-				{
-					if (t[1].x == t[1].y)
-						put_pixel(xy->x, xy->y, ~object->color, img);
-					else
-						put_pixel(xy->x, xy->y, object->color, img);
-				}
 				*object_no = i;
 			}
 		}
-		if (t[0].x == 10000 && t[0].y == 10000)
-			put_pixel(xy->x, xy->y, 0x444444, img);
 		i++;
 		objects = objects->next;
 	}
@@ -129,12 +118,12 @@ static t_3f	intersect(t_utils *utils, t_3f *ray, t_3f *ray_origin, t_img *img, t
 		printf("POINT_HIT: %f %f %f\n", point_hit->x, point_hit->y, point_hit->z);
 		printf("NORMAL: %f %f %f\n", normal.x, normal.y, normal.z);
 		if (utils->closest_object != NULL)
-			printf("CLOSEST OBJECT: %d\n", utils->closest_object->type);
+			printf("CLOSEST OBJECT TYPE: %d\n", utils->closest_object->type);
 	}
 	return (normal);
 }
 
-static int	intersect_light(t_utils *utils, t_3f *ray, t_3f *ray_origin, t_3f *light_hit)
+static int	intersect_light(t_utils *utils, t_3f *ray, t_3f *ray_origin)
 {
 	t_list		*objects;
 	t_object	*object;
@@ -157,7 +146,13 @@ static int	intersect_light(t_utils *utils, t_3f *ray, t_3f *ray_origin, t_3f *li
 	{
 		ret = 0;
 		object = (t_object *)objects->content;
-		if (object->type == 1)
+		if (object->type == 0)
+		{
+			obj.light = (t_light *)object->content;
+			origin = subtract_vectors(object->origin, *ray_origin);
+			ret = intersect_sphere(ray, &origin, 0.1f, &t[1]);
+		}
+		else if (object->type == 1)
 		{
 			obj.sphere = (t_sphere *)object->content;
 			origin = subtract_vectors(object->origin, *ray_origin);
@@ -184,8 +179,6 @@ static int	intersect_light(t_utils *utils, t_3f *ray, t_3f *ray_origin, t_3f *li
 		{
 			if (t[1].x < t[0].x)
 			{
-				*light_hit = scale_vector(*ray, (float)t[1].x);
-				*light_hit = add_vectors(*light_hit, *ray_origin);
 				object_no = i;
 				t[0] = t[1];
 			}
@@ -201,7 +194,7 @@ void	ray_plotting(t_utils *utils, t_img *img, t_2i coords)
 	t_list		*objects;
 	t_object	*object;
 	t_3f		point_hit;
-	t_3f		light_hit;
+	int			light_no;
 	t_2f		scrn;
 	t_3f		normal;
 	t_3f		ray;
@@ -220,12 +213,16 @@ void	ray_plotting(t_utils *utils, t_img *img, t_2i coords)
 	scrn.x = (float)(2 * coords.x) / (float)img->dim.width - 1.0f;
 	scrn.y = (float)(-2 * coords.y) / (float)img->dim.height + 1.0f;
 	ray = get_ray(scrn, &utils->cam, &utils->proj);
+	utils->closest_object = NULL;
 	normal = intersect(utils, &ray, &utils->cam.origin, img, &(t_2i){coords.x, coords.y}, &point_hit, &object_no[0]);
-	if (utils->closest_object != NULL)
+	light_no = 1;
+	if (utils->closest_object == NULL)
 	{
-		seperate_rgb(utils->closest_object->color, &rgb.x, &rgb.y, &rgb.z);
+		put_pixel(coords.x, coords.y, 0x000000, img);
+		return ;
 	}
-	if (utils->render == 1)
+	seperate_rgb(utils->closest_object->color, &rgb.x, &rgb.y, &rgb.z);
+	if (utils->render == 1 && utils->closest_object->type != 0)
 	{
 		objects = utils->objects;
 		while (objects != NULL)
@@ -238,12 +235,13 @@ void	ray_plotting(t_utils *utils, t_img *img, t_2i coords)
 				light_color.x = (float)temp_rgb.x / 255;
 				light_color.y = (float)temp_rgb.y / 255;
 				light_color.z = (float)temp_rgb.z / 255;
-				obj.light->dir = subtract_vectors(point_hit, object->origin);
+				obj.light->dir = subtract_vectors(object->origin, point_hit);
 				t = sqrt(((obj.light->dir.x) * (obj.light->dir.x)) + ((obj.light->dir.y) * (obj.light->dir.y)) + ((obj.light->dir.z) * (obj.light->dir.z)));
-				t = t / obj.light->lumen;
 				obj.light->dir = normalize_vector(obj.light->dir);
-				object_no[1] = intersect_light(utils, &obj.light->dir, &object->origin, &light_hit);
-				obj.light->dir = scale_vector(obj.light->dir, -1.0f);
+				//object_no[1] = intersect_light(utils, &obj.light->dir, &object->origin, &light_hit);
+				point_hit = add_vectors(point_hit, scale_vector(obj.light->dir, 0.0001f));
+				object_no[1] = intersect_light(utils, &obj.light->dir, &point_hit);
+				t = t / obj.light->lumen;
 				/*if (normal.x == 0.0f && normal.y == 0.0f && normal.z == 0.0f)
 					 light_level = 1.0f;
 				else
@@ -254,7 +252,8 @@ void	ray_plotting(t_utils *utils, t_img *img, t_2i coords)
 				light_level -= t;
 				if (light_level < 0.0)
 					light_level = 0.0;
-				if (object_no[0] == object_no[1] && light_level > 0 && object_no[0] > 0 && fabs(light_hit.x - point_hit.x) < 0.1 && fabs(light_hit.z - point_hit.z) < 0.1 && fabs(light_hit.y - point_hit.y) < 0.1)
+				//if (object_no[0] == object_no[1] && light_level > 0 && object_no[0] > 0)
+				if (object_no[1] == light_no)
 				{
 					rgb_t.x += (int)(rgb.x * light_level * light_color.x);
 					rgb_t.y += (int)(rgb.y * light_level * light_color.y);
@@ -268,12 +267,17 @@ void	ray_plotting(t_utils *utils, t_img *img, t_2i coords)
 					printf("t / lumen ratio: %lf\n", t);
 					printf("normal & light similarity: %lf\n", (double)dot_product(normal, obj.light->dir));
 					printf("light level %%: %lf\n", light_level);
-					printf("OBJECT NO: %d | %d\n", object_no[0], object_no[1]);
+					printf("OBJECT NO: %d LIGHT NO: %d\n", object_no[1], light_no);
 				}
 			}
+			light_no += 1;
 			objects = objects->next;
 		}
 		put_pixel(coords.x, coords.y, combine_rgb(rgb_t.x, rgb_t.y, rgb_t.z), img);
+	}
+	else
+	{
+			put_pixel(coords.x, coords.y, utils->closest_object->color, img);
 	}
 }
 
