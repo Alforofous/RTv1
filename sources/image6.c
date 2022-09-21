8a878eb97702a64c76617e195a81ebf170dd0e72
Alforofus <davidm.22@hotmail.com>
2022-09-21 16:57:47 +0300

    Fixed a bug of plus sign in add_object button was misaligned.

--- sources/image6.c
+++ sources/image6.c
@@ -6,7 +6,7 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 12:16:07 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/08/22 12:27:25 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:56:43 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 void	draw_image6(t_utils *utils)
 {
-	t_2i	coords[2];
+	t_3i	coords;
 
-	coords[0].x = (int)(utils->curr_img->dim.width * 0.5);
-	coords[0].y = (int)(utils->curr_img->dim.height * 0.0);
-	coords[1].x = (int)(utils->curr_img->dim.width * 0.5);
-	coords[1].y = (int)(utils->curr_img->dim.height * 1.0);
+	coords.x = (int)((float)utils->curr_img->dim.width * 0.3f);
+	coords.y = (int)((float)utils->curr_img->dim.height * 0.5f);
+	coords.z = (int)((float)utils->curr_img->dim.width * 0.7f);
 	draw_rectf(&(t_pxl_func){&put_pixel, utils->curr_img},
 		&(t_2i){0, 0}, &(t_2i){utils->curr_img->dim.width - 2,
 		utils->curr_img->dim.height - 2}, 0x42CD00);
@@ -27,9 +26,12 @@ void	draw_image6(t_utils *utils)
 		&(t_2i){0, 0}, &(t_2i){utils->curr_img->dim.width - 2,
 		utils->curr_img->dim.height - 2}, 0xFFFFFF);
 	draw_line(&(t_pxl_func){&put_dot, utils},
-		&(t_line){coords[0].x, coords[0].y, coords[1].x, coords[1].y},
+		&(t_line){coords.x, coords.y, coords.z, coords.y},
 		0xFFFFFF, 0xFFFFFF);
+	coords.x = (int)((float)utils->curr_img->dim.height * 0.3f);
+	coords.y = (int)((float)utils->curr_img->dim.width * 0.5f);
+	coords.z = (int)((float)utils->curr_img->dim.height * 0.7f);
 	draw_line(&(t_pxl_func){&put_dot, utils},
-		&(t_line){coords[0].y, coords[0].x, coords[1].y, coords[1].x},
+		&(t_line){coords.y, coords.x, coords.y, coords.z},
 		0xFFFFFF, 0xFFFFFF);
 }
