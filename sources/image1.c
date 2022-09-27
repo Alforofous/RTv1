205430533ee6600053a09e953b1a4f98ccc10721
David Malesevic <dmalesev@c1r6p8.hive.fi>
2022-09-27 17:27:34 +0300

    Camera origin displays now with 1 decimal point instead of 3.

--- sources/image1.c
+++ sources/image1.c
@@ -6,7 +6,7 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 10:44:56 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/09/27 17:10:22 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/09/27 17:26:43 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ static void	display_origin(t_utils *utils, t_2i coords, t_ray *cam)
 	color.y = 0xFFFFFF;
 	font_height = (int)utils->font2->bound_box[1];
 	offset = display_str(&utils->pxl[0], coords, "X: ", color);
-	display_float(utils, offset, (t_2f){cam->origin.x, 5.0f}, color);
+	display_float(utils, offset, (t_2f){cam->origin.x, 1.0f}, color);
 	coords.y += (int)font_height;
 	offset = display_str(&utils->pxl[0], coords, "Y: ", color);
-	display_float(utils, offset, (t_2f){cam->origin.y, 5.0f}, color);
+	display_float(utils, offset, (t_2f){cam->origin.y, 1.0f}, color);
 	coords.y += (int)font_height;
 	offset = display_str(&utils->pxl[0], coords, "Z: ", color);
-	display_float(utils, offset, (t_2f){cam->origin.z, 5.0f}, color);
+	display_float(utils, offset, (t_2f){cam->origin.z, 1.0f}, color);
 }
 
 void	draw_image1(t_utils *utils)
@@ -44,6 +44,7 @@ void	draw_image1(t_utils *utils)
 	display_int(utils, coords, (int)utils->proj.fov, color);
 	coords.x = (int)(utils->curr_img->dim.width * 0.0);
 	coords.y = (int)(utils->curr_img->dim.height * 0.1);
+	coords = display_str(&utils->pxl[0], coords, "x", color);
 	display_float(utils, coords, (t_2f){utils->multiplier, 1.0f}, color);
 	coords.x = (int)(utils->curr_img->dim.width * 0.0);
 	coords.y = (int)(utils->curr_img->dim.height * 0.15);
