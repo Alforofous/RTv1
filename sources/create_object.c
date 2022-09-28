406efbaaf95374e1f0ea34942c3527e1c81fa43b
Alforofus <davidm.22@hotmail.com>
2022-09-28 16:57:24 +0300

    Rewrote and renamed object_properties to create object.

---
+++ sources/create_object.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   object_properties.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 13:50:04 by dmalesev          #+#    #+#             */
+/*   Updated: 2022/09/28 16:23:19 by dmalesev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+t_object	create_light(t_3f origin, t_uint color, float lumen)
+{
+	t_object	object;
+
+	object.type = 0;
+	object.origin = origin;
+	object.color = color;
+	object.lumen = lumen;
+	return (object);
+}
+
+t_object	create_sphere(t_3f origin, t_uint color, float radius)
+{
+	t_object	object;
+
+	object.type = 1;
+	object.origin = origin;
+	object.color = color;
+	object.radius = radius;
+	return (object);
+}
+
+t_object	create_plane(t_3f origin, t_uint color, t_3f axis)
+{
+	t_object	object;
+
+	object.type = 2;
+	object.origin = origin;
+	object.color = color;
+	object.axis = normalize_vector(axis);
+	return (object);
+}
+
+t_object	create_cone(t_3f origin, t_uint color, t_3f axis, float radius)
+{
+	t_object	object;
+
+	object.type = 3;
+	object.origin = origin;
+	object.color = color;
+	object.axis_length = vector_magnitude(axis);
+	object.axis = normalize_vector(axis);
+	object.radius = radius;
+	return (object);
+}
+
+t_object	create_cylinder(t_3f origin, t_uint color, t_3f axis, float radius)
+{
+	t_object	object;
+
+	object.type = 4;
+	object.origin = origin;
+	object.color = color;
+	object.axis_length = vector_magnitude(axis);
+	object.axis = normalize_vector(axis);
+	object.radius = radius;
+	return (object);
+}
