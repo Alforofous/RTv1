af1c8e394c73f6a52f2a6b0f461aaaa65fada5b8
Alforofus <davidm.22@hotmail.com>
2022-09-28 09:49:40 +0300

    Mouse scroll now moves selected object forward/back.

--- sources/scroll_mouse.c
+++ sources/scroll_mouse.c
@@ -6,30 +6,37 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 15:52:22 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/06/13 12:10:55 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/09/28 09:47:34 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-void	scroll_wheel(t_utils *u, int x, int y)
+void	scroll_wheel(t_utils *utils, int x, int y)
 {
-	u += 0;
+	utils += 0;
 	x += 0;
 	y += 0;
 }
 
-void	scroll_wheel_up(t_utils *u, int x, int y)
+void	scroll_wheel_up(t_utils *utils, int x, int y)
 {
-	if (u->mouse.zoom > 3)
-		u->mouse.zoom = (int)((float)u->mouse.zoom / 1.6f);
+	if (utils->sel_object != NULL)
+	{
+		utils->sel_object->origin = add_vectors(utils->sel_object->origin, scale_vector(utils->cam.dir.forward, utils->multiplier));
+		render_screen(utils);
+	}
 	x += 0;
 	y += 0;
 }
 
-void	scroll_wheel_down(t_utils *u, int x, int y)
+void	scroll_wheel_down(t_utils *utils, int x, int y)
 {
-	u->mouse.zoom = (int)((float)u->mouse.zoom * 1.6f);
+	if (utils->sel_object != NULL)
+	{
+		utils->sel_object->origin = add_vectors(utils->sel_object->origin, scale_vector(utils->cam.dir.back, utils->multiplier));
+		render_screen(utils);
+	}
 	x += 0;
 	y += 0;
 }
