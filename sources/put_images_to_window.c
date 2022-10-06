a73e4ac7d0a13587ced954ac2d4cdb9db2507075
David Malesevic <dmalesev@c1r6p9.hive.fi>
2022-10-06 12:42:39 +0300

    Split put screen function into it's own file. Now called put images to window.

---
+++ sources/put_images_to_window.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_images_to_screen.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 12:28:32 by dmalesev          #+#    #+#             */
+/*   Updated: 2022/10/06 12:37:27 by dmalesev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+static void	put_main_images(t_utils *utils)
+{
+	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[0].ptr,
+		utils->img[0].dim.start.x, utils->img[0].dim.start.y);
+	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[1].ptr,
+		utils->img[1].dim.start.x, utils->img[1].dim.start.y);
+}
+
+static void	put_selected_object_images(t_utils *utils)
+{
+	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[3].ptr,
+		utils->img[3].dim.start.x, utils->img[3].dim.start.y);
+	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[6].ptr,
+		utils->img[6].dim.start.x, utils->img[6].dim.start.y);
+	if (utils->property[0] != NULL)
+	{
+		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[9].ptr,
+			utils->img[9].dim.start.x, utils->img[9].dim.start.y);
+		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[10].ptr,
+			utils->img[10].dim.start.x, utils->img[10].dim.start.y);
+	}
+	if (utils->property[1] != NULL)
+	{
+		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[11].ptr,
+			utils->img[11].dim.start.x, utils->img[11].dim.start.y);
+		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[12].ptr,
+			utils->img[12].dim.start.x, utils->img[12].dim.start.y);
+	}
+}
+
+static void	put_misc_images(t_utils *utils)
+{
+	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[5].ptr,
+		utils->img[5].dim.start.x, utils->img[5].dim.start.y);
+	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[7].ptr,
+		utils->img[7].dim.start.x, utils->img[7].dim.start.y);
+	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[8].ptr,
+		utils->img[8].dim.start.x, utils->img[8].dim.start.y);
+	if (utils->add_object_menu == 1)
+		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[4].ptr,
+			utils->img[4].dim.start.x, utils->img[4].dim.start.y);
+	if (utils->visual_rays >= 1)
+		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[2].ptr,
+			utils->img[2].dim.start.x, utils->img[2].dim.start.y);
+}
+
+void	put_images_to_window(t_utils *utils)
+{
+	put_main_images(utils);
+	if (utils->sel_object != NULL)
+		put_selected_object_images(utils);
+	put_misc_images(utils);
+}
--- sources/render_screen.c
+++ sources/render_screen.c
@@ -6,42 +6,11 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 16:10:14 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/10/06 10:21:08 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/10/06 12:40:19 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
-
-void	put_screen(t_utils *utils)
-{
-	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[0].ptr,
-		utils->img[0].dim.start.x, utils->img[0].dim.start.y);
-	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[1].ptr,
-		utils->img[1].dim.start.x, utils->img[1].dim.start.y);
-	if (utils->visual_rays >= 1)
-		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[2].ptr,
-			utils->img[2].dim.start.x, utils->img[2].dim.start.y);
-	if (utils->sel_object != NULL)
-	{
-		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[3].ptr,
-			utils->img[3].dim.start.x, utils->img[3].dim.start.y);
-		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[6].ptr,
-			utils->img[6].dim.start.x, utils->img[6].dim.start.y);
-		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[9].ptr,
-			utils->img[9].dim.start.x, utils->img[9].dim.start.y);
-		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[10].ptr,
-			utils->img[10].dim.start.x, utils->img[10].dim.start.y);
-	}
-	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[5].ptr,
-		utils->img[5].dim.start.x, utils->img[5].dim.start.y);
-	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[7].ptr,
-		utils->img[7].dim.start.x, utils->img[7].dim.start.y);
-	mlx_put_image_to_window(utils->mlx, utils->win, utils->img[8].ptr,
-		utils->img[8].dim.start.x, utils->img[8].dim.start.y);
-	if (utils->add_object_menu == 1)
-		mlx_put_image_to_window(utils->mlx, utils->win, utils->img[4].ptr,
-			utils->img[4].dim.start.x, utils->img[4].dim.start.y);
-}
 
 void	image_processing(t_utils *utils, t_img *img, t_uint fill_color)
 {
@@ -52,7 +21,6 @@ void	image_processing(t_utils *utils, t_img *img, t_uint fill_color)
 	mlx_put_image_to_window(utils->mlx, utils->win, img->ptr, img->dim.start.x,
 		img->dim.start.y);
 }
-
 
 void	render_screen(t_utils *utils)
 {
@@ -76,5 +44,5 @@ void	render_screen(t_utils *utils)
 	image_processing(utils, &utils->img[8], 0x000000);
 	if (utils->visual_rays >= 1)
 		image_processing(utils, &utils->img[2], 0x67000000);
-	put_screen(utils);
+	put_images_to_window(utils);
 }
