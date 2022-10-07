911005c921bb3451f9f612409236a8861601e5ab
David Malesevic <dmalesev@c1r6p9.hive.fi>
2022-10-07 10:48:42 +0300

    for last commit.

---
+++ sources/draw_rgb_slider.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   image5.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 16:41:44 by dmalesev          #+#    #+#             */
+/*   Updated: 2022/10/07 10:44:11 by dmalesev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+static void	rgb_iter(t_img *img, t_uint (*f)(t_img *, t_2i *))
+{
+	t_2i	coords;
+
+	coords.y = 0;
+	while (coords.y <= img->dim.size.y)
+	{
+		coords.x = 0;
+		while (coords.x <= img->dim.size.x)
+		{
+			put_pixel(coords.x, coords.y, (*f)(img, &coords), img);
+			coords.x += 1;
+		}
+		coords.y += 1;
+	}
+}
+
+void	draw_rgb_slider(void *param)
+{
+	t_utils	*utils;
+	t_img	*img;
+	t_2i	coords;
+
+	utils = param;
+	img = &utils->img[5];
+	rgb_iter(img, &rgb_slider);
+	coords = (t_2i){img->dim.size.x - 2, img->dim.size.y - 2};
+	draw_rect(&(t_pxl_func){&put_dot, img}, (t_2i){0, 0}, coords, 0xFFFFFF);
+}
